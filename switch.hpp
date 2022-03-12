f9c86dd66996ca7aaf7292df327e8aab5b780145
Antony Peacock <ant.peacock@gmail.com>
2022-03-12 08:04:59 +0000

    Initial unit tests

--- switch.hpp
+++ switch.hpp
@@ -24,18 +24,29 @@ constexpr auto switchTag(auto case_) { return switchCaseElement(case_, boost::ha
 #define SWITCH_MAX 64
 #endif
 
-inline constexpr auto defaultCase = [](auto unhandledValue){ return unhandledValue; };
+inline constexpr auto defaultCase = [](auto unhandledValue){};
 
-// We accept any hana Product (of case × tag), and also single element serving as both case and tag (see below).
 template<class CT, CT C, class T = std::integral_constant<CT, C>>
 using SwitchCase = boost::hana::pair<std::integral_constant<CT, C>, T>;
 
 namespace detail { 
 
-#define SWITCH_CASE(Z,N,_)      \
-    case decltype(switchCondition(boost::mp11::mp_at_c<CaseList, N>{}))::value:                     \
-        return std::forward<CaseHandler>(caseHander)(decltype(switchCondition(boost::mp11::mp_at_c<CaseList, N>{})){});
 
+#define SWITCH_CASE(Z,N,_)                                                                                              \
+    case decltype(switchCondition(boost::mp11::mp_at_c<CaseList, N>{}))::value:                                         \
+        return std::forward<CaseHandler>(caseHander)(decltype(switchTag(boost::mp11::mp_at_c<CaseList, N>{})){});
+
+template<class Result, class Condition, class DefaultHandler>
+Result invokeDefaultHandler(Condition condition, DefaultHandler defaultHandler)
+{
+    if constexpr (!std::is_void_v<Result> && std::is_void_v<std::invoke_result_t<DefaultHandler, Condition>>)
+    {
+        defaultHandler(condition);
+        __builtin_unreachable();
+    }
+    else
+        return defaultHandler(condition);
+}
 
 #define SWITCH_OVERLOAD(Z,N,_)                                                                                          \
 template<class Result, class Condition, class CaseList, class CaseHandler, class DefaultCase>                           \
@@ -51,34 +62,40 @@ constexpr Result switch_(                                                       
     {                                                                                                                   \
         BOOST_PP_REPEAT_ ## Z(N, SWITCH_CASE, ~)                                                                        \
     }                                                                                                                   \
-    return defaultCase(condition);                                                                                      \
+    return invokeDefaultHandler<Result, Condition, DefaultCase>(condition, defaultCase);                                \
 }
 
 BOOST_PP_REPEAT(SWITCH_MAX, SWITCH_OVERLOAD, ~)
 #undef SWITCH_CASE
 #undef SWITCH_OVERLOAD
 
+template<class... T> struct SwitchCommonReference {
+    using type = std::conditional_t<(std::is_rvalue_reference_v<T> && ...), std::add_rvalue_reference_t<std::common_type_t<T...>>,
+                 std::conditional_t<(std::is_reference_v<T> && ...), std::add_lvalue_reference_t<std::common_type_t<T...>>,
+                 std::common_type_t<T...>>>;
+};    
+
 template<class Condition, class CaseList, class CaseHandler, class DefaultHandler>
 auto switchResult(CaseList caseList)
 {
     constexpr std::size_t Size = boost::mp11::mp_size<CaseList>::value;
     if constexpr (Size == 0u)
-        return std::invoke_result_t<DefaultHandler, Condition>{};
+        return std::invoke_result<DefaultHandler, Condition>{};
     else
         return [&]<std::size_t... I>(std::index_sequence<I...>)
         {
-            return std::common_type_t<
+            return SwitchCommonReference<
                 std::invoke_result_t<CaseHandler, decltype(switchTag(boost::mp11::mp_at_c<CaseList, I>{}))>...>{};
         }(std::make_index_sequence<Size>{});
 }
 
 template<class Condition, class CaseList, class CaseHandler, class DefaultHandler>
-using SwitchResult = decltype(switchResult<Condition, CaseList, CaseHandler, DefaultHandler>(std::declval<CaseList>()));
+using SwitchResult = typename decltype(switchResult<Condition, CaseList, CaseHandler, DefaultHandler>(std::declval<CaseList>()))::type;
 
 }
 
 template<class Condition, class CaseList, class CaseHandler, class DefaultCase = decltype(defaultCase)>
-constexpr auto switch_(
+constexpr detail::SwitchResult<Condition, CaseList, CaseHandler, DefaultCase> switch_(
     Condition const condition, /// The switch conditional value to be executed.
     CaseList const caseList, /// List of case predicates.
     CaseHandler&& caseHander, /// Handler for case statement bodies. 
--- switch.test.cpp
+++ switch.test.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch.hpp>
 
 #include <array>
+#include <concepts>
 #include <cstdint>
 
 enum class Colours : std::uint32_t
@@ -19,14 +20,66 @@ static_assert(Switch::detail::ExtractNumParameters<Vector<int, float>>::value ==
 static_assert(Switch::detail::ExtractNumParameters<Vector<int, int, int>>::value == 3, "Increse SWITCH_MAX");
 */
 
+template<typename Type, class... X>
+concept AllOf = (std::same_as<Type, X> && ...);
+
+struct ReturnLValueReference
+{
+    struct Tag {} b, c, i, f;
+    Tag& operator()(bool) { return b; }
+    Tag& operator()(char) { return c; }
+    Tag& operator()(int) { return i; }
+    Tag& operator()(float) { return f; }
+};
+
+TEST_CASE("Support functional switches that returns references", "[switch.returns_references]")
+{
+    using Cases =   Switch::CaseList<
+                        Switch::SwitchCase<std::size_t, 0, bool>, 
+                        Switch::SwitchCase<std::size_t, 1, char>, 
+                        Switch::SwitchCase<std::size_t, 2, int>,
+                        Switch::SwitchCase<std::size_t, 3, float>
+                    >;
+
+    ReturnLValueReference retRef;
+    ReturnLValueReference::Tag& result = Switch::switch_(4, Cases{}, std::ref(retRef));// std::ref(retRef));
+    REQUIRE(&result == &retRef.f);
+}
+
+struct ReturnRValueReference
+{
+    struct Tag {} b, c, i, f;
+    Tag&& operator()(bool) { return std::move(b); }
+    Tag&& operator()(char) { return std::move(c); }
+    Tag&& operator()(int) { return std::move(i); }
+    Tag&& operator()(float) { return std::move(f); }
+};
+
+TEST_CASE("Support functional switches that returns rvalue references", "[switch.returns_rvalue_references]")
+{
+    using Cases =   Switch::CaseList<
+                        Switch::SwitchCase<std::size_t, 0, bool>, 
+                        Switch::SwitchCase<std::size_t, 1, char>, 
+                        Switch::SwitchCase<std::size_t, 2, int>,
+                        Switch::SwitchCase<std::size_t, 3, float>
+                    >;
+
+    ReturnRValueReference retRef;
+    ReturnRValueReference::Tag&& result = Switch::switch_(4, Cases{}, std::ref(retRef));// std::ref(retRef));
+    REQUIRE(&result == &retRef.f);
+}
+
 TEST_CASE("Support functional switches with sequential enums", "[switch.sequential_enums]")
 {
-    constexpr std::array colours = { Colours::Red, Colours::Green, Colours::Blue };
-    using Cases =   Switch::CaseList<
-                        Switch::SwitchCase<Colours, Colours::Red>, 
-                        Switch::SwitchCase<Colours, Colours::Green>, 
-                        Switch::SwitchCase<Colours, Colours::Blue>
-                    >;
+/*    constexpr std::array colours = { Colours::Red, Colours::Green, Colours::Blue };
+    constexpr auto buildSwitchCases = []<typename T, T... t>(std::integral_constant<T, t>... cases) 
+    {
+        return Switch::CaseList<
+                    Switch::SwitchCase<typename decltype(cases)::type, decltype(cases)::value>...
+                >{};
+    };
+
+    using Cases = decltype(buildSwitchCases.template operator()<Colours>(Colours::Red, Colours::Green, Colours::Blue));
 
 
     for (auto colour : colours)
@@ -42,7 +95,7 @@ TEST_CASE("Support functional switches with sequential enums", "[switch.sequenti
 //                std::cout << "Default case" << std::endl;
 //            }
         );
-    }
+    }*/
 }
 
 
